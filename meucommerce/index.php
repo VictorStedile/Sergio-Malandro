ae8cb73f8b81bfc2bc10b1ef2f0a6c3ce41990a4
Victor Stedile <stedilev8@gmail.com>
2022-10-06 13:54:36 -0300

    A

--- meucommerce/index.php
+++ meucommerce/index.php
@@ -3,28 +3,10 @@
 
 <div>
     <div style="height: 100px; background-color: lightblue; text-align: center">
-        <a href="?" class='menu'>Home</a>
-        <a href="?pagina=listaprodutos" class='menu'>Listagem de produtos</a>
-        <a href="?pagina=listafornecedores" class='menu'>Listagem de fornecedores</a>
-        <a href="?pagina=cadastropro" class='menu'>Cadastro de produto</a>
-        <a href="?pagina=cadastrofor" class='menu'>Cadastro de fornecedor</a>
+        
     </div>
     <div style="height: 589px; widhth: 100%; background-color: lightyellow">
-        <center>
-        <?php 
-            if(isset($_GET['pagina'])){
-                if(file_exists($_GET['pagina'] . '.php')){
-                    include($_GET['pagina'] . '.php');
-                }
-                else {
-                    include('error.php');
-                }
-            }
-            else{
-                include('informacoes.php');
-            }
-        ?>
-        </center>
+        
     </div>
     <div style="height: 100px; background-color: lightblue">
             
